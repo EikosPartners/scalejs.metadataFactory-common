3df1c91e602e25de367f2f5d17330c5c0665000c
Draisy <draisy.sabel@eikospartners.com>
2016-06-29 12:12:11 -0400

    build files

--- dist/setValue/setValueViewModel.js
+++ dist/setValue/setValueViewModel.js
@@ -18,12 +18,12 @@ function setValueViewModel(node) {
         context.data.subscribe(function (data) {
             var dict = context.dictionary();
             Object.keys(dict).forEach(function (node) {
-                setValue(dict[node], data);
+                _setValue(dict[node], data);
             });
         });
     }
 
-    function setValue(node, data, clear) {
+    function _setValue(node, data, clear) {
         if (node.id && node.setValue && (data.hasOwnProperty(node.id) || clear)) {
             node.setValue(data[node.id]); //pass as object with value key?
         }
