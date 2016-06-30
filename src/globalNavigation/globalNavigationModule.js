1d53ab4a0298b11d5d1d56f22aea433660b7f498
Dan Tanzer <dan.tanzer@eikospartners.com>
2016-06-30 09:29:19 -0400

    remove unused imports

--- src/globalNavigation/globalNavigationModule.js
+++ src/globalNavigation/globalNavigationModule.js
@@ -3,9 +3,6 @@ import globalNavigationView from './globalNavigation.html';
 import globalNavigationBindings from './globalNavigationBindings';
 import { registerTemplates, registerBindings } from 'scalejs.mvvm';
 import { registerViewModels } from 'scalejs.metadataFactory';
-import {navigation, layout} from 'scalejs.navigation';
-import ko from 'knockout';
-import dataservice from 'dataservice';
 
 registerTemplates(globalNavigationView);
 registerBindings(globalNavigationBindings);
