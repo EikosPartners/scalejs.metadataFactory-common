215c5da48e719b1fab12716becd8844e0e0c1d8b
egucciar <egucciar@gmail.com>
2016-06-30 10:48:47 -0400

    Dist folder

--- dist/globalNavigation/globalNavigationModule.js
+++ dist/globalNavigation/globalNavigationModule.js
@@ -1,5 +1,9 @@
 'use strict';
 
+var _scalejs = require('scalejs.mvvm');
+
+var _scalejs2 = require('scalejs.metadataFactory');
+
 var _globalNavigationViewModel = require('./globalNavigationViewModel.js');
 
 var _globalNavigationViewModel2 = _interopRequireDefault(_globalNavigationViewModel);
@@ -11,20 +15,6 @@ var _globalNavigation2 = _interopRequireDefault(_globalNavigation);
 var _globalNavigationBindings = require('./globalNavigationBindings');
 
 var _globalNavigationBindings2 = _interopRequireDefault(_globalNavigationBindings);
-
-var _scalejs = require('scalejs.mvvm');
-
-var _scalejs2 = require('scalejs.metadataFactory');
-
-var _scalejs3 = require('scalejs.navigation');
-
-var _knockout = require('knockout');
-
-var _knockout2 = _interopRequireDefault(_knockout);
-
-var _dataservice = require('dataservice');
-
-var _dataservice2 = _interopRequireDefault(_dataservice);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
