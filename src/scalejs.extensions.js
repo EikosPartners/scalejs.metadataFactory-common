59f6ded2bef3b15131febdb93ae3557547fb7c5e
Dan Tanzer <dan.tanzer@eikospartners.com>
2016-07-05 15:35:04 -0400

    remove unused packages from extension file

--- src/scalejs.extensions.js
+++ src/scalejs.extensions.js
@@ -1,9 +1,5 @@
 import mvvm from 'scalejs.mvvm';
-import ko from 'knockout';
-import 'scalejs.metadataFactory';
-import 'scalejs.navigation';
-import 'scalejs.noticeboard';
-import 'scalejs.expression-jsep';
 
 console.log('loading mvvm.init');
+
 mvvm.init({doNotRender: true});
