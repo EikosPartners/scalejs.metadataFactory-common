9e6ed15a73c1be931ac6f829907b8c592da15be0
egucciar <egucciar@gmail.com>
2016-07-11 17:12:01 -0400

    Fix index.html

--- src/scalejs.metadataFactory-common.js
+++ src/scalejs.metadataFactory-common.js
@@ -4,3 +4,10 @@ import './adapter/adapterModule';
 import './input/inputModule';
 import './store/storeModule';
 import './template/templateModule';
+
+import mvvm from 'scalejs.mvvm';
+
+import ko from 'knockout';
+window.ko = ko;
+
+mvvm.init({});
