8be72ad50b0291bd5e8a553a56beaedbe7ee9d87
francomendoza <franco.mendoza@eikospartners.com>
2016-07-11 17:04:06 -0400

    add list advanced test for footer

--- test/tests/listAdvancedSpec.js
+++ test/tests/listAdvancedSpec.js
@@ -1,15 +1,16 @@
-import { getRegisteredTypes, createViewModel } from 'scalejs.metadataFactory';
+import { getRegisteredTypes, createViewModel, createViewModels } from 'scalejs.metadataFactory';
 import 'chai';
 
 import 'listAdvanced/listAdvancedModule';
 import 'list/listModule';
 
 describe('listAdvancedModule test', function () {
-    it('registers the listAdvanced viewModel', function () {
+    it('registers the listAdvanced viewModel', function (done) {
         expect(getRegisteredTypes()).to.include('listAdvanced');
+        done();
     });
 
-    it('creates the listAdvancedViewModel', function () {
+    it('creates the listAdvancedViewModel', function (done) {
         const list = createViewModel({
             "type": "listAdvanced",
             "list": {
@@ -18,5 +19,44 @@ describe('listAdvancedModule test', function () {
         });
         expect(list).to.have.property('rows');
         expect(list).to.have.property('groups');
-    })    
+        done();
+    });
+
+    it('creates the listAdvancedViewModel with footer with list-items and standard input types', function (done) {
+        const list = createViewModels([
+            {
+              "type": "listAdvanced",
+              "footers": [
+                  {
+                      "items": [
+                          {
+                              "type": "EMPTY"
+                          },
+                          {
+                              "type": "ADD"
+                          },
+                          {
+                              "type": "TEXT"
+                          },
+                          {
+                              "type": "input",
+                              "inputType": "text"
+                          },
+                          {
+                              "type": "TOTAL"
+                          }
+                      ]
+                  }
+              ],
+              "list": {
+                  "type": "list"
+              }
+          }
+        ])[0];
+
+        expect(list.footers[0].items.length).to.equal(5);
+        done();
+    });
+
+
 });
