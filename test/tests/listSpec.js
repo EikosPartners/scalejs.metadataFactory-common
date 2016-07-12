d95ca0a86cab6a7fa45167807a25b647ed43456f
francomendoza <franco.mendoza@eikospartners.com>
2016-07-12 15:04:43 -0400

    set readonly test for list

--- test/tests/listSpec.js
+++ test/tests/listSpec.js
@@ -4,19 +4,22 @@ import 'chai';
 import 'input/inputModule';
 import 'list/listModule';
 
-describe('listModule test', function () {
-    it('registers the list viewModel', function () {
+describe('listModule test', function (done) {
+    it('registers the list viewModel', function (done) {
         expect(getRegisteredTypes()).to.include('list');
+        done();
     });
 
-    it('creates the listViewModel', function () {
+    it('creates the listViewModel', function (done) {
         const list = createViewModel({
             "type": "list"
         });
         expect(list).to.have.property('rows');
+
+        done();
     });
 
-    it('tests readonly listViewModel item functionality with isNew', function () {
+    it('tests readonly listViewModel item functionality with isNew', function (done) {
         const list = createViewModels([{
             "type": "list",
             "items": [{
@@ -42,8 +45,38 @@ describe('listModule test', function () {
 
         expect(list.rows()).to.have.length(2);
         expect(list.rows()[0].items()[0].context.getValue('isNew')).to.equal(true);
-        expect(list.rows()[0].items()[0].readonly()).to.equal(true);        
+        expect(list.rows()[0].items()[0].readonly()).to.equal(true);
+
+        done();
     });
 
-    it('tests unique');
+    it('exposes a setReadonly function that toggles all rows readonly', function (done) {
+      const list = createViewModels([{
+          "type": "list",
+          "items": [{
+              "type": "input",
+              "id": "test",
+              "inputType": "text",
+              "options": {
+                  "readonly": "isNew"
+              }
+          }],
+          "data": [
+              {
+                  "test": "initialValue"
+              }
+          ]
+      }])[0];
+
+      list.add();
+      list.setReadonly(true);
+      
+      list.rows().forEach((row) => {
+        row.items().forEach((item) => {
+          expect(item.readonly()).to.equal(true);
+        });
+      });
+
+      done();
+    });
 });
