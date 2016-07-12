9a020e928bae1c6329db373d93aa5dd174e270e4
Draisy <draisy.sabel@eikospartners.com>
2016-07-12 13:04:52 -0400

    build files

--- dist/scalejs.metadataFactory-common.js
+++ dist/scalejs.metadataFactory-common.js
@@ -11,4 +11,18 @@ require('./input/inputModule');
 require('./store/storeModule');
 
 require('./template/templateModule');
+
+var _scalejs = require('scalejs.mvvm');
+
+var _scalejs2 = _interopRequireDefault(_scalejs);
+
+var _knockout = require('knockout');
+
+var _knockout2 = _interopRequireDefault(_knockout);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+window.ko = _knockout2.default;
+
+_scalejs2.default.init({});
 //# sourceMappingURL=scalejs.metadataFactory-common.js.map
