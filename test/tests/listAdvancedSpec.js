2a151b7b8534b19326bcaead396d1e4e313d6d27
francomendoza <franco.mendoza@eikospartners.com>
2016-07-12 12:17:58 -0400

    add list autocomplete unique test

--- test/tests/listAdvancedSpec.js
+++ test/tests/listAdvancedSpec.js
@@ -1,8 +1,9 @@
 import { getRegisteredTypes, createViewModel, createViewModels } from 'scalejs.metadataFactory';
 import 'chai';
-
+import noticeboard from 'scalejs.noticeboard';
 import 'listAdvanced/listAdvancedModule';
 import 'list/listModule';
+import 'input/inputModule';
 
 describe('listAdvancedModule test', function () {
     it('registers the listAdvanced viewModel', function (done) {
@@ -58,5 +59,66 @@ describe('listAdvancedModule test', function () {
         done();
     });
 
+    it('list with unique autocomplete removes values from options as they are chosen', function (done) {
+      noticeboard.set("test", [
+        {
+          "key": "store_a",
+          "value": "store_b"
+        },
+        {
+          "key": "store_a_2",
+          "value": "store_b_2"
+        }
+      ]);
+
+      const listJSON = {
+          "type": "listAdvanced",
+          "id": "testAdvancedList",
+          "list": {
+              "type": "list",
+              "label": "test",
+              "items": [
+                  {
+                      "id": "testInput",
+                      "type": "input",
+                      "inputType": "autocomplete",
+                      "autocompleteSource": {
+                          "fromArray": "store.test"
+                      },
+                      "keyMap": {
+                          "textKey": "key",
+                          "valueKey": "value"
+                      },
+                      "options": {
+                          "unique": true
+                      }
+                  },
+                  {
+                      "type": "DELETE_FLAG",
+                      "visible": "!readonly",
+                      "id": "DeleteIndicator"
+                  }
+              ]
+          }
+      };
+
+      let listViewModel = createViewModels([listJSON])[0];
+
+      listViewModel.setValue([{ testInput: "store_b" }]);
+      listViewModel.list.add();
+
+      expect(listViewModel.rows()[0].items()[0].autocompleteSource()).to.deep.equal([
+        {
+          label: "store_a_2",
+          value: "store_b_2",
+          original: {
+            key: "store_a_2",
+            value: "store_b_2"
+          }
+        }
+      ]);
+
+      done();
+    });
 
 });
