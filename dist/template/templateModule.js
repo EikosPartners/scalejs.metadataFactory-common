b192dc1e62a3753620a68b71b07eefcaaaf9d8b8
egucciar <egucciar@gmail.com>
2016-08-02 11:51:02 -0400

    up version

--- dist/template/templateModule.js
+++ dist/template/templateModule.js
@@ -8,7 +8,13 @@ var _templateViewModel = require('./templateViewModel');
 
 var _templateViewModel2 = _interopRequireDefault(_templateViewModel);
 
+var _template = require('./template.html');
+
+var _template2 = _interopRequireDefault(_template);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+(0, _scalejs.registerTemplates)(_template2.default);
 
 (0, _scalejs2.registerViewModels)({
     template: _templateViewModel2.default
