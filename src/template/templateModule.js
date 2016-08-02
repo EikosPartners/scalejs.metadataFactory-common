e6db6877b017f5f8b5f3f165c782c71aa180e09e
egucciar <egucciar@gmail.com>
2016-08-02 11:48:12 -0400

    Added div template

--- src/template/templateModule.js
+++ src/template/templateModule.js
@@ -2,6 +2,9 @@ import { registerTemplates, registerBindings } from 'scalejs.mvvm';
 import { registerViewModels } from 'scalejs.metadataFactory';
 
 import viewModel from './templateViewModel';
+import view from './template.html';
+
+registerTemplates(view);
 
 registerViewModels({
     template: viewModel
