86a1992bd1205caca0eb20371841d587f320925e
Zachary Donato <zdonato@stevens.edu>
2016-08-09 14:48:26 -0400

    Changed redirect to run target through mustache.

--- src/action/actions/redirect.js
+++ src/action/actions/redirect.js
@@ -1,6 +1,24 @@
-import { registerActions } from '../actionModule';
+import { registerActions } from 'scalejs.metadatafactory-common/dist/action/actionModule';
+import { unwrap } from 'knockout';
+import { merge } from 'scalejs';
+import mustache from 'mustache';
+import ko from 'knockout';
 
 function redirect(options) {
-    window.location.replace(options.target);
+    if (!options.target) {
+        console.error("Must provide target!");
+        return;
+    }
+
+    let data = unwrap(options.data || (this && this.data)),
+        params;
+
+    if (options.params && options.paramsKey) {
+        data = merge(data, options[options.paramsKey]);
+    }
+
+    var url = mustache.render(options.target, data);
+
+    window.location.replace(url);
 }
 registerActions({redirect});
