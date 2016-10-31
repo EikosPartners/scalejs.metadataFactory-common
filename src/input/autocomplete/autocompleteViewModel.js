db13fc608da7d3320491cd19abdf069c1ea6abdd
Gucciardo <erica.gucciardo@eikospartners.com>
2016-10-31 16:46:42 -0400

    linting input module

--- src/input/autocomplete/autocompleteViewModel.js
+++ src/input/autocomplete/autocompleteViewModel.js
@@ -1,186 +1,188 @@
-import { observable, observableArray, computed, unwrap } from 'knockout';
-import { createViewModels as createViewModelsUnbound } from 'scalejs.metadataFactory';
+import { observableArray, computed } from 'knockout';
 import { evaluate } from 'scalejs.expression-jsep';
-import { merge, has, is } from 'scalejs';
+import { has } from 'scalejs';
 import dataservice from 'dataservice';
 import _ from 'lodash';
 
 
-//TODO: Refactor multi-input functionality out or to its own viewmodel
+// TODO: Refactor multi-input functionality out or to its own viewmodel
 
-    export default function autocompleteViewModel(node, inputViewModel) {
-        var context = this,
-            dataSourceEndpoint = node.dataSourceEndpoint,
-            keyMap = node.keyMap || {},
-            createViewModels = createViewModelsUnbound.bind(this),
-            // inputViewModel
-            inputValue = inputViewModel.inputValue,
-            subs = inputViewModel.subs,
-            mapItem = inputViewModel.mapItem,
-            hasFocus = inputViewModel.hasFocus,
-            readonly = inputViewModel.readonly,
-            isShown = inputViewModel.isShown,            
-            autocompleteSource = inputViewModel.values, //todo: just use values
-            // props
-            sourceArray,
-            validations,
-            options = node.options || {},
-            unique = options.unique,
-            computedSource,
-            itemMapper = mapItem(keyMap),
-            objectValue,
-            autocompleteSourceDef;
-        
-        if(node.autocompleteSource) {
-            console.warn('[autocomplete] please move the autocompleteSource into options');
-            autocompleteSourceDef = _.cloneDeep(node.autocompleteSource);
-        } else {
-            autocompleteSourceDef = _.cloneDeep(node.options && node.options.autocompleteSource)
-        }
+export default function autocompleteViewModel(node, inputViewModel) {
+    const context = this,
+        dataSourceEndpoint = node.dataSourceEndpoint,
+        keyMap = node.keyMap || {},
+        options = node.options || {},
+        unique = options.unique,
+        // inputViewModel
+        inputValue = inputViewModel.inputValue,
+        subs = inputViewModel.subs,
+        mapItem = inputViewModel.mapItem,
+        itemMapper = mapItem(keyMap),
+        // todo: just use values
+        autocompleteSource = inputViewModel.values;
+        // props
+    let sourceArray,
+        validations,
+        computedSource,
+        autocompleteSourceDef;
 
-        function mapAutocompleteSource(source) {
-            return source.map(function(src) {
-                if (typeof src === 'string') {
-                    return {
-                        value: src,
-                        label: src
-                    }
-                } else {
-                    return src;
-                }
-            });
-        }
+    if (node.autocompleteSource) {
+        console.warn('[autocomplete] please move the autocompleteSource into options');
+        autocompleteSourceDef = _.cloneDeep(node.autocompleteSource);
+    } else {
+        autocompleteSourceDef = _.cloneDeep(node.options && node.options.autocompleteSource);
+    }
 
-        function getAutocompleteSource() {
-            dataservice.ajax(dataSourceEndpoint, function(error, data) {
-                if (error) {
-                    console.error('Data retrieval failure', error);
-                    return;
-                }
-                if (keyMap.dataKey) {
-                    sourceArray  =  data[keyMap.dataKey];
-                    // todo: update to use mapItem
-                    var mappedData = data[keyMap.dataKey].map(function(d) {
-                        return keyMap.textKey && keyMap.valueKey ?
-                            {
-                                label: (Array.isArray(keyMap.textKey) ? keyMap.textKey : [keyMap.textKey]).map(function(k) {
-                                    return d[k];
-                                }).join(keyMap.delimiter || ' / '),
-                                value: d[keyMap.valueKey]
-                            }
-                            : d[node.id]; //todo: remove this and add mapping!
-                    })
-                    autocompleteSource(mapAutocompleteSource(_.uniqBy(mappedData, function(item) {
-                            return item ?  has(item, 'value') ? item.value : item : '';
-                    }).filter(Boolean))) // remove empty values
-                } else {
-                    sourceArray = data.SearchResults;
-                    autocompleteSource(mapAutocompleteSource(data.SearchResults));
-                }
-            });
-        }
-
-        function getAutocompleteSourceFromContext() {
-            var source = _.toArray(evaluate(autocompleteSourceDef.fromArray || [], context.getValue));
-
-            // storing source array before any mapping
-            sourceArray = source;
-            if (Array.isArray(source)) {
-                autocompleteSource(
-                    _.uniqBy(
-                        source
-                            .map(itemMapper)
-                            //todo: remove additional mapping - using binding options
-                            .map(function(item) {
-                                return {
-                                    label: item.text,
-                                    value: item.value,
-                                    original: item.original
-                                };
-                            }),
-                        function(item) {
-                            return item.value;
-                        }
-                    )
-                );
+    function mapAutocompleteSource(source) {
+        return source.map((src) => {
+            if (typeof src === 'string') {
+                return {
+                    value: src,
+                    label: src
+                };
             }
-        }
+            return src;
+        });
+    }
 
-        // function setReadonly(bool) {
-        //     readonly(bool);
-        // }
-
-        if (dataSourceEndpoint) {
-            subs.push(computed(getAutocompleteSource));
-        }
-
-        if (Array.isArray(autocompleteSourceDef)) {
-            sourceArray = autocompleteSourceDef;
-            autocompleteSource(mapAutocompleteSource(autocompleteSourceDef));
-        }
-
-        if (autocompleteSourceDef && !Array.isArray(autocompleteSourceDef)) {
-            subs.push(computed(getAutocompleteSourceFromContext).extend({ deferred: true }));
-        }
-
-        if (!options.addNew) {
-            validations = _.cloneDeep(options.validations) || {};
-            if (!validations || !validations.autocomplete ) {
-                validations.autocomplete = {
-                    message: 'Please choose a valid selection from the options.',
-                    params: autocompleteSource
-                }
-            } else {
-                validations.autocomplete = {
-                    message: validations.autocomplete.message || 'Please choose a valid selection from the options.',
-                    params: autocompleteSource
-                }
+    function getAutocompleteSource() {
+        dataservice.ajax(dataSourceEndpoint, (error, data) => {
+            if (error) {
+                console.error('Data retrieval failure', error);
+                return;
             }
-        }
-
-        if (unique) {
-
-            inputValue.subscribe(function (oldValue) {
-                context.unique[node.id].remove(oldValue);
-            }, null, 'beforeChange');
-            inputValue.subscribe(function (newValue) {
-                if(context.deleteFlag && context.deleteFlag()) { return; }
-                context.unique[node.id].push(newValue);
-            });
-
-            if(context.deleteFlag) {
-                context.deleteFlag.subscribe(function(deleted) {
-                    if (deleted) {
-                        context.unique[node.id].remove(inputValue());
+            if (keyMap.dataKey) {
+                sourceArray = data[keyMap.dataKey];
+                // todo: update to use mapItem
+                const mappedData = data[keyMap.dataKey].map((d) => {
+                    const retVal = keyMap.textKey && keyMap.valueKey ?
+                    {
+                        label: (Array.isArray(keyMap.textKey)
+                            ? keyMap.textKey
+                            : [keyMap.textKey]).map(k => d[k])
+                            .join(keyMap.delimiter || ' / '),
+                        value: d[keyMap.valueKey]
                     }
+                    : d[node.id]; // todo: remove this and add mapping!
+                    return retVal;
                 });
+                autocompleteSource(
+                    mapAutocompleteSource(_.uniqBy(mappedData, (item) => {
+                        let retVal = '';
+                        if (item && has(item, 'value')) {
+                            retVal = item.value;
+                        }
+                        return retVal;
+                    }).filter(Boolean))); // remove empty values
+            } else {
+                sourceArray = data.SearchResults;
+                autocompleteSource(mapAutocompleteSource(data.SearchResults));
             }
+        });
+    }
 
-            computedSource = computed({
-                read: function () {
-                    var selectedItems = _.difference(context.unique[node.id](), [inputValue()]).map(function (item) {
+    function getAutocompleteSourceFromContext() {
+        let source = _.toArray(evaluate(autocompleteSourceDef.fromArray || [], context.getValue));
+
+        // storing source array before any mapping
+        sourceArray = source;
+        if (Array.isArray(source)) {
+            autocompleteSource(
+                _.uniqBy(
+                    source
+                        .map(itemMapper)
+                        // todo: remove additional mapping - using binding options
+                        .map((item) => {
                             return {
-                                value: item
+                                label: item.text,
+                                value: item.value,
+                                original: item.original
                             };
                         }),
-                        newSource = _.differenceBy(autocompleteSource(), selectedItems, 'value');
-                    return newSource;
-                },
-                write: function (newValues) {
-                    autocompleteSource(newValues);
-                }
-            }).extend({ deferred: true });
+                    (item) => {
+                        return item.value;
+                    }
+                )
+            );
         }
+    }
 
-        return {
-            autocompleteSource: unique ? computedSource : autocompleteSource,
-            validations,
-            mappedChildNodes: observableArray(), // todo: still need?
-            // setReadonly: setReadonly,
-            dispose: () => {
-                if(unique) {
+    // function setReadonly(bool) {
+    //     readonly(bool);
+    // }
+
+    if (dataSourceEndpoint) {
+        subs.push(computed(getAutocompleteSource));
+    }
+
+    if (Array.isArray(autocompleteSourceDef)) {
+        sourceArray = autocompleteSourceDef;
+        autocompleteSource(mapAutocompleteSource(autocompleteSourceDef));
+    }
+
+    if (autocompleteSourceDef && !Array.isArray(autocompleteSourceDef)) {
+        subs.push(computed(getAutocompleteSourceFromContext).extend({ deferred: true }));
+    }
+
+    if (!options.addNew) {
+        validations = _.cloneDeep(options.validations) || {};
+        if (!validations || !validations.autocomplete) {
+            validations.autocomplete = {
+                message: 'Please choose a valid selection from the options.',
+                params: autocompleteSource
+            };
+        } else {
+            validations.autocomplete = {
+                message: validations.autocomplete.message || 'Please choose a valid selection from the options.',
+                params: autocompleteSource
+            };
+        }
+    }
+
+    if (unique) {
+        inputValue.subscribe((oldValue) => {
+            context.unique[node.id].remove(oldValue);
+        }, null, 'beforeChange');
+        inputValue.subscribe((newValue) => {
+            if (context.deleteFlag && context.deleteFlag()) { return; }
+            context.unique[node.id].push(newValue);
+        });
+
+        if (context.deleteFlag) {
+            context.deleteFlag.subscribe((deleted) => {
+                if (deleted) {
                     context.unique[node.id].remove(inputValue());
                 }
+            });
+        }
+
+        computedSource = computed({
+            read: function () {
+                let selectedItems = _.difference(context.unique[node.id](), [inputValue()]).map((item) => {
+                        return {
+                        value: item
+                    };
+                    }),
+                    newSource = _.differenceBy(autocompleteSource(), selectedItems, 'value');
+                return newSource;
+            },
+            write: function (newValues) {
+                autocompleteSource(newValues);
+            }
+        }).extend({ deferred: true });
+    }
+
+    console.log('Autocomplete source array:', sourceArray);
+
+    return {
+        autocompleteSource: unique ? computedSource : autocompleteSource,
+        validations,
+        mappedChildNodes: observableArray(), // todo: still need?
+        // setReadonly: setReadonly,
+        dispose: () => {
+            if (unique) {
+                context.unique[node.id].remove(inputValue());
             }
         }
     };
+}
--- src/input/validation/validationEngine.js
+++ src/input/validation/validationEngine.js
@@ -2,77 +2,76 @@ import { evaluate } from 'scalejs.expression-jsep';
 import ko from 'knockout';
 import _ from 'lodash';
 import 'knockout.validation';
-       
-    /*
-    * made validation engine its own file to support more complex logic
-    */
 
-    //set the generic validation on the knockout binding
-    ko.validation.init({
-        insertMessages: false,
-        messagesOnModified: true,
-        decorateElement: false,
-        decorateElementOnModified: false,
-        decorateInputElement: false
-    });
+/*
+* made validation engine its own file to support more complex logic
+*/
 
-    ko.validation.rules['expression'] = {
-        validator: function (val, p) {
-            var params = p.params ? p.params : p,
-                getValue = params[1],
-                validator = this;
-            // option 1: params[0] is not an array so it is just the term
-            if(!Array.isArray(params[0])) {
-                return evaluate.apply(null, params);
-            }
-            // option 2: params[0] is an array so it is many terms
-            return params[0].every(function (e) {
-                var isValid = evaluate.call(null, e.term, getValue);
-                if(!isValid) {
-                    validator.message = e.message;
-                }
-                return isValid;
-            });
-        },
-        message: 'Expression is not correct'
-    };
+// set the generic validation on the knockout binding
+ko.validation.init({
+    insertMessages: false,
+    messagesOnModified: true,
+    decorateElement: false,
+    decorateElementOnModified: false,
+    decorateInputElement: false
+});
 
-    ko.validation.rules['autocomplete'] = {
-        validator: function (val, p) {
-           if(!val){
-               return true; //if the value is undefined, then we ignore the validation.
-           }
-
-           return _.findIndex(p, ['value', val]) > -1;
-        },
-        message: 'Expression is not correct'
-    };
-
-    ko.validation.rules['customError'] = {
-        validator: function (val, error) {
-            if (error) {
-                this.message = error;
-                return false;
-            }
-            return true;
-        },
-        message: 'Value is not valid'
-    };
-    
-    var patternValidator = ko.validation.rules['pattern'].validator;
-    ko.validation.rules['pattern'].validator = function (val, params) {
-        var validator = this;
-        if (Array.isArray(params)) {
-            return params.every(function (param) {
-                var isValid = patternValidator(val, param.params);
-                if (!isValid) {
-                    validator.message = param.message;
-                }
-                return isValid;
-            });
+ko.validation.rules.expression = {
+    validator: function (val, p) {
+        const params = p.params ? p.params : p,
+            getValue = params[1],
+            validator = this;
+        // option 1: params[0] is not an array so it is just the term
+        if (!Array.isArray(params[0])) {
+            return evaluate(...params);
         }
-        return patternValidator(val, params);
-    }   
-    
-    ko.validation.registerExtenders();
+        // option 2: params[0] is an array so it is many terms
+        return params[0].every((e) => {
+            const isValid = evaluate.call(null, e.term, getValue);
+            if (!isValid) {
+                validator.message = e.message;
+            }
+            return isValid;
+        });
+    },
+    message: 'Expression is not correct'
+};
 
+ko.validation.rules.autocomplete = {
+    validator: function (val, p) {
+        if (!val) {
+            return true; // if the value is undefined, then we ignore the validation.
+        }
+
+        return _.findIndex(p, ['value', val]) > -1;
+    },
+    message: 'Expression is not correct'
+};
+
+ko.validation.rules.customError = {
+    validator: function (val, error) {
+        if (error) {
+            this.message = error;
+            return false;
+        }
+        return true;
+    },
+    message: 'Value is not valid'
+};
+
+const patternValidator = ko.validation.rules.pattern.validator;
+ko.validation.rules.pattern.validator = function (val, params) {
+    const validator = this;
+    if (Array.isArray(params)) {
+        return params.every((param) => {
+            const isValid = patternValidator(val, param.params);
+            if (!isValid) {
+                validator.message = param.message;
+            }
+            return isValid;
+        });
+    }
+    return patternValidator(val, params);
+};
+
+ko.validation.registerExtenders();
