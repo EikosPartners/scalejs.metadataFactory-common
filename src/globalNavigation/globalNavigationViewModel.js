6863dc1e2bc61c57eed2f788a17a5efb48e22436
Gucciardo <erica.gucciardo@eikospartners.com>
2016-11-15 15:13:52 -0500

    comment out log statement

--- src/globalNavigation/globalNavigationViewModel.js
+++ src/globalNavigation/globalNavigationViewModel.js
@@ -3,29 +3,32 @@ import { navigation, layout } from 'scalejs.navigation';
 import dataservice from 'dataservice';
 import { merge } from 'scalejs';
 import ko from 'knockout';
+import _ from 'lodash';
 
 export default function globalNavigation(node) {
-    var routes = ko.observableArray(node.routes),
+    const routes = ko.observableArray(node.routes),
         navLinks = navigation.navLinks,
         activeLink = navigation.activeLink;
 
     function walkGetTypes(nodes) {
         return (nodes || [])
-            .reduce( (types, node) => types.concat([node.type])
-            .concat(walkGetTypes(node.children)), []);
+            .reduce((types, childNode) => types.concat([childNode.type])
+            .concat(walkGetTypes(childNode.children)), []);
     }
 
-    routes().forEach(function (route) {
-        navigation.addNav(route, function (routeInfo) {
-            var name = route.get.replace('{path}', routeInfo.path ? '_' + routeInfo.path.replace('/','_') : '');
-            dataservice.ajax({ 'uri': name }, function (err, metadata) {
+    routes().forEach((route) => {
+        navigation.addNav(route, (routeInfo) => {
+            const name = route.get.replace('{path}', routeInfo.path ? `_${routeInfo.path.replace('/', '_')}` : '');
+            dataservice.ajax({ uri: name }, (err, metadata) => {
                 if (err) {
                     return;
                 }
-                var types = _.uniq(walkGetTypes(Array.isArray(metadata) ? metadata : [metadata])).filter(function (type) {
-                    return type && getRegisteredTypes().indexOf(type) === -1
-                });
-                console.log('Missing types:', types);
+                const types = _.uniq(walkGetTypes(Array.isArray(metadata) ? metadata : [metadata]))
+                    .filter(type =>
+                        type && getRegisteredTypes().indexOf(type) === -1
+                    );
+
+                // console.log('Missing types:', types);
 
                 layout.content(metadata);
             });
@@ -34,15 +37,15 @@ export default function globalNavigation(node) {
 
     navigation.init(node.initial || 0);
 
-    routes.subscribe(function (oldRoutes) {
-        oldRoutes.forEach(routeOptions => {
+    routes.subscribe((oldRoutes) => {
+        oldRoutes.forEach((routeOptions) => {
             navigation.removeNav(routeOptions.text);
         });
     }, null, 'beforeChange');
 
     return merge(node, {
-        navLinks: navLinks,
-        activeLink: activeLink,
+        navLinks,
+        activeLink,
         dispose: function () {
             routes([]);
         }
