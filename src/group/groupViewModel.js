24a3805085867188c13f82ac869a3a7fe587a8a9
Gucciardo <ADCWTMV@ams.bnymellon.net>
2017-02-02 19:42:51 -0500

    added initial:true handling to group and list

--- src/group/groupViewModel.js
+++ src/group/groupViewModel.js
@@ -1,60 +1,56 @@
 import { observable } from 'knockout';
 import { has, merge } from 'scalejs';
-import { createViewModels } from 'scalejs.metadataFactory'
+import { createViewModels } from 'scalejs.metadataFactory';
 
-    function createNodeDictionary(mappedChildNodes) {
-        var dictionary = {};
+function createNodeDictionary(mappedChildNodes) {
+    const dictionary = {};
 
-        function addToDictionary(node) {
-            if(node.id) {
-                dictionary[node.id] = node;
-            }
-            if (!node.getValue) {
-                (node.mappedChildNodes || []).forEach(addToDictionary);
-            }
+    function addToDictionary(node) {
+        if (node.id) {
+            dictionary[node.id] = node;
         }
-        (mappedChildNodes || []).forEach(addToDictionary);
-
-        return dictionary;
+        if (!node.getValue) {
+            (node.mappedChildNodes || []).forEach(addToDictionary);
+        }
     }
+    (mappedChildNodes || []).forEach(addToDictionary);
 
-    export default function (node) {
-        var options = node.options || {},
-            dictionary = observable(),
-            context = this,
-            mappedChildNodes;
+    return dictionary;
+}
 
-        function setValue(values) {
-            var value = has(values, 'value') ? values.value : values;
-            Object.keys(dictionary()).forEach(function(id) {
-                var child = dictionary()[id];
-                if(child.setValue && value.hasOwnProperty(child.id)) {
-                    child.setValue(value[child.id]);
-                }
-            });
-        }
-
-        function getValue() {
-            var ret = Object.keys(dictionary()).reduce(function (obj, id) {
-                var child = dictionary()[id];
-                if(child.getValue) {
-                    obj[child.id] = child.getValue();
-                }
-                return obj;
-            }, {});
-            return ret;
-        }
-
+export default function (node) {
+    const dictionary = observable(),
+        context = this,
         mappedChildNodes = createViewModels.call(this, node.children);
 
-        dictionary(createNodeDictionary(mappedChildNodes));
-
-        return merge(node, {
-            mappedChildNodes,
-            dictionary,
-            setValue,
-            getValue,
-            context
+    function setValue(values, opts) {
+        const value = has(values, 'value') ? values.value : values;
+        Object.keys(dictionary()).forEach((id) => {
+            const child = dictionary()[id];
+            if (child.setValue && Object.prototype.isPrototypeOf.call(value, child.id)) {
+                child.setValue(value[child.id], opts);
+            }
         });
-    };
+    }
 
+    function getValue() {
+        const ret = Object.keys(dictionary()).reduce((obj, id) => {
+            const child = dictionary()[id];
+            if (child.getValue) {
+                obj[child.id] = child.getValue();
+            }
+            return obj;
+        }, {});
+        return ret;
+    }
+
+    dictionary(createNodeDictionary(mappedChildNodes));
+
+    return merge(node, {
+        mappedChildNodes,
+        dictionary,
+        setValue,
+        getValue,
+        context
+    });
+}
